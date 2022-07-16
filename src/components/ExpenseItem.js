00241774ffa7ccb26c44b4cd43339e77f8ef3d69
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-07-16 13:26:28 +0430

    🔧 refactor: adding a little bit of margin between elements of list

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,16 +2,16 @@ import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
 	return (
-		<div class="col-12 g-0 row border border-info rounded p-3">
-			<div class="col-xxl-10 col-xl-10 col-lg-10 col-md-9 col-sm-8 col-8">
-				<div class="row">
-					<div class="fs-4">{props.title}</div>
+		<div className="col-12 g-0 row border border-info rounded p-3 my-1">
+			<div className="col-xxl-10 col-xl-10 col-lg-10 col-md-9 col-sm-8 col-8">
+				<div className="row">
+					<div className="fs-4">{props.title}</div>
 					<ExpenseDate date={props.date} />
 				</div>
 			</div>
-			<div class="col">
-				<div class="d-grid gap-2 h-100">
-					<button class="btn btn-primary fs-5">${props.cost}</button>
+			<div className="col">
+				<div className="d-grid gap-2 h-100">
+					<button className="btn btn-primary fs-5">${props.cost}</button>
 				</div>
 			</div>
 		</div>
